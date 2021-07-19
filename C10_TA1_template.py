b4ba4fcb4d947bf5de8ac7da3c7334d909a5ebf2
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-19 13:51:58 +0530

    Update C10_TA1_template.py

--- C10_TA1_template.py
+++ C10_TA1_template.py
@@ -7,12 +7,14 @@ screen = pygame.display.set_mode((width,height))
 images={}
 images["bg1"] = pygame.image.load("D:/Flappy for AI/bg1.png").convert_alpha()
 images["base"] = pygame.image.load("D:/Flappy for AI/base.png").convert_alpha()
+## Space for the code to be written ##
 while True:
+    screen.fill((50,150,255))
+    screen.blit(images["bg1"],[0,0])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.fill((50,150,255))
-    screen.blit(images["bg1"],[0,0])
+   
     screen.blit(images["base"],[0,550])
     pygame.display.update()
     
