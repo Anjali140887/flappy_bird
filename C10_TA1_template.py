25f467de804766c3087264bfe223e44eb2f89920
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-19 13:00:48 +0530

    Add files via upload

---
+++ C10_TA1_template.py
@@ -0,0 +1,19 @@
+import pygame, sys
+pygame.init()
+clock=pygame.time.Clock()
+width=400
+height=600
+screen = pygame.display.set_mode((width,height))
+images={}
+images["bg1"] = pygame.image.load("C:/Users/NASCA/Desktop/Flappy for AI/Flappy for AI/bg1.png").convert_alpha()
+images["base"] = pygame.image.load("C:/Users/NASCA/Desktop/Flappy for AI/Flappy for AI/base.png").convert_alpha()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill((50,150,255))
+    screen.blit(images["bg1"],[0,0])
+    screen.blit(images["base"],[0,550])
+    pygame.display.update()
+    
+pygame.quit()
