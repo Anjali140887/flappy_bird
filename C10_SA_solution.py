5a92a7bd4fa5a63c0db5b36593dc1595ceb1d1c8
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-19 16:31:11 +0530

    Update C10_SA_solution.py

--- C10_SA_solution.py
+++ C10_SA_solution.py
@@ -5,10 +5,10 @@ width=400
 height=600
 screen = pygame.display.set_mode((width,height))
 images={}
-images["bg1"] = pygame.image.load("C:/Users/NASCA/Desktop/Flappy for AI/Flappy for AI/bg1.png").convert_alpha()
-images["base"] = pygame.image.load("C:/Users/NASCA/Desktop/Flappy for AI/Flappy for AI/base.png").convert_alpha()
+images["bg1"] = pygame.image.load("D:/Flappy for AI/bg1.png").convert_alpha()
+images["base"] = pygame.image.load("D:/Flappy for AI/base.png").convert_alpha()
 # New addition
-images["bird"] = pygame.image.load("C:/Users/NASCA/Desktop/Flappy for AI/Flappy for AI/bird.png").convert_alpha()
+images["bird"] = pygame.image.load("D:/Flappy for AI/bird.png").convert_alpha()
 class Bird:
     r=pygame.Rect(100,250,30,30)
     speed=0
