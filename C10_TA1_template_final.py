e4834d194094b0dd1b67e481ce2b2bcad41812e1
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-20 14:15:49 +0530

    Add files via upload

---
+++ C10_TA1_template_final.py
@@ -0,0 +1,14 @@
+
+#Import pygame
+
+#Initialize pygame
+ 
+#Create a screen
+
+#Set a caption to the screen
+
+#Begin the game loop
+
+    
+#Quit the game outside the loop   
+{"mode":"full","isActive":false}
