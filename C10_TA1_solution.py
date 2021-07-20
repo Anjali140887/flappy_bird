4f5f53f3d441b9286ca5cd88ece4559d2ef410dc
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-20 14:50:57 +0530

    Update C10_TA1_solution.py

--- C10_TA1_solution.py
+++ C10_TA1_solution.py
@@ -7,12 +7,10 @@ screen = pygame.display.set_mode((width,height))
 ## Space for the code to be written ##
 while True:
     screen.fill((50,150,255))
-    screen.blit(images["bg1"],[0,0])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
    
-    screen.blit(images["base"],[0,550])
     pygame.display.update()
     
 pygame.quit()
