a9082010ee0cf81aed3c9194b17a8adeb7e0dd52
Anjali140887 <86819899+Anjali140887@users.noreply.github.com>
2021-07-21 12:51:03 +0530

    Update TA4_solution_final_C9.py

--- TA4_solution_final_C9.py
+++ TA4_solution_final_C9.py
@@ -10,11 +10,12 @@ images["base"] = pygame.image.load("D:/Flappy for AI/base.png").convert_alpha()
 while True:
     screen.fill((50,150,255))
     screen.blit(images["bg1"],[0,0])
+    screen.blit(images["base"],[0,550])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     
-    screen.blit(images["base"],[0,550])
+   
     pygame.display.update()
     
 pygame.quit()
